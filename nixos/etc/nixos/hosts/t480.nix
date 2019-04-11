815a912552f7b2e5d4896f8b5a075ed0b1ab04c1
siers <wimuan@gmail.com>
2019-04-11 12:53:17 +0300

    Autocommit on 2019-04-11 12:53:17: 3 files changed, 86 insertions(+)

---
+++ nixos/etc/nixos/hosts/t480.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+  [
+    ./inspiron-hardware.nix
+    # ../lib/work-development-forwarder.nix
+  ];
+
+  networking.hostName = "t480";
+
+  hardware.cpu.intel.updateMicrocode = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = [
+    { name = "enc-vg"; device = "/dev/disk/by-uuid/a4ee2444-0bc4-4d6c-9754-c7f2353cca35"; preLVM = true; allowDiscards = true; } # /dev/sda3
+    { name = "enc-home"; device = "/dev/disk/by-uuid/79e9c6d6-6c41-4951-95c2-a98dafe11507"; preLVM = true; allowDiscards = true; } # /dev/sda4
+  ];
+
+  fileSystems = [
+    { mountPoint = "/"; device = "/dev/vg/root"; fsType = "ext4"; options = ["noatime"]; }
+    { mountPoint = "/boot"; device = "/dev/sda2"; fsType = "vfat"; options = ["noatime" "nofail"]; }
+    { mountPoint = "/home"; device = "/dev/mapper/enc-home"; fsType = "ext4"; options = ["noatime" "nofail"]; }
+    { mountPoint = "/tmp"; device = "tmpfs"; fsType = "tmpfs"; options = ["nosuid" "nodev" "relatime"]; }
+  ] ++ import ../lib/syncthing.nix;
+
+  swapDevices = [ { device = "/dev/vg/swap"; } ];
+
+  services = (import ../lib/xserver.nix).xfce-i3;
+
+  environment.systemPackages = (import ../lib/package-sets.nix { inherit pkgs; }).everything;
+}
---
+++ nixos/etc/nixos/lib/packages/xclip-for-mac.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, base, bytestring, process, stdenv, fetchgit }:
+mkDerivation {
+  pname = "xclip";
+  version = "0.1.0.0";
+  src = fetchgit {
+    rev = "59f3d3bdc01df6be666e5d5cd9f95fa3d4443c9d";
+    url = "https://github.com/siers/xclip-for-mac";
+    sha256 = "0fbbi1zapla8b0ybllwsz1akb6aldqggjb0m5a54qmsbx4sbynvr";
+  };
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [ base bytestring process ];
+  description = "xclip for mac";
+  license = "unlicense";
+  platforms = stdenv.lib.platforms.darwin;
+}
